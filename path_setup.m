c03af788cab8e5445cc826e65da5c7f400e8f58f
Gerasimos01 <acivil@hotmail.gr>
2020-02-20 13:10:06 +0200

    Update path_setup.m

--- path_setup.m
+++ path_setup.m
@@ -1,3 +1,10 @@
+%bool runCluster = false; 
 
+%        //public string exampleOutputPathGen = @"C:\Users\acivi\Documents\notes_elegxoi\REFERENCE_kanonikh_gewmetria_fe2_post_dg\examples\example1\input_matlab";
+
+%        public string exampleOutputPathGen { get { if (runCluster) { return @"C:\Users\cluster\Documents\Large_rves\examples\example3\input_matlab"; }
+
+
+%example_path='C:\Users\acivi\Documents\notes_elegxoi\REFERENCE_kanonikh_gewmetria_fe2_post_dg\examples\example1';
 example_path='C:\Users\acivi\Documents\notes_elegxoi\REFERENCE_kanonikh_gewmetria_fe2_post_dg\examples\example1';
-addpath('C:\Users\acivi\Documents\auto_matlab\common_functions3');
+%addpath('C:\Users\acivi\Documents\auto_matlab\common_functions3');
